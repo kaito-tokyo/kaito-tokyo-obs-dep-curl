b55cd90ba29d6966878e4db32f96973a34c1c13d
Kaito Udagawa <umireon@gmail.com>
2024-04-22 00:30:22 +0900

    Fix

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # kaito-tokyo-obs-dep-libcurl
+
+## Build instructions for macOS
+
+```
+curl -LO https://github.com/curl/curl/archive/refs/tags/curl-8_7_1.tar.gz
+tar -xf curl-8_7_1.tar.gz
+./build-macos.sh
+```
+
+## Build instructions for Windows
+
+```
+curl -LO https://github.com/curl/curl/archive/refs/tags/curl-8_7_1.tar.gz
+tar -xf curl-8_7_1.tar.gz
+./build-macos.sh
+```
---
+++ build-macos.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -euo pipefail
+
+CONFIG="${1?}"
+VERSION="${2?}"
+CURL_DIR="${3?}"
+
+cmake "$CURL_DIR" -B "build_$CONFIG" \
+  -DCMAKE_BUILD_TYPE="$CONFIG" \
+  -DCURL_USE_OPENSSL=OFF \
+  -DCURL_USE_SECTRANSP=ON \
+  -DBUILD_CURL_EXE=OFF \
+  -DBUILD_SHARED_LIBS=OFF \
+  -DHTTP_ONLY=ON \
+  -DCURL_USE_LIBSSH2=OFF \
+  -DBUILD_TESTING=OFF \
+  -DPICKY_COMPILER=OFF
+
+cmake --build "build_$CONFIG"
+cmake --install "build_$CONFIG" --prefix "release/$CONFIG"
+tar -C "release/$CONFIG" -cvf "release/libcurl-macos-$VERSION-$CONFIG.tar.gz" .
