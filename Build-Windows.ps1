1797009d399ddda6f27019f3dc1d8672fa3b89d1
Kaito Udagawa <umireon@gmail.com>
2024-04-22 00:43:59 +0900

    Add build script (#1)
    * Fix
    
    * Update build.yml
    
    * Fix
    
    * Fix

---
+++ Build-Windows.ps1
@@ -0,0 +1,16 @@
+Param($Configuration, $Version, $CurlDirectory)
+
+cmake "$CurlDirectory" -B "build_$Configuration" `
+  -DCMAKE_BUILD_TYPE="$Configuration" `
+  -DCURL_USE_OPENSSL=OFF `
+  -DCURL_USE_SCHANNEL=ON `
+  -DBUILD_CURL_EXE=OFF `
+  -DBUILD_SHARED_LIBS=OFF `
+  -DHTTP_ONLY=ON `
+  -DCURL_USE_LIBSSH2=OFF `
+  -DBUILD_TESTING=OFF `
+  -DPICKY_COMPILER=OFF `
+
+cmake --build "build_$Configuration" --config "$Configuration"
+cmake --install "build_$Configuration" --config "$Configuration" --prefix "release/$Configuration"
+Compress-Archive "release\$Configuration\*" "release\libcurl-windows-$Version-$Configuration.zip" -Verbose
